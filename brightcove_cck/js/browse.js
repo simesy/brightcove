c9c015150305da8349031a8dcc4f3a6a8430947c
Jakub Suchy <meba@31977.no-reply.drupal.org>
2010-05-22 10:03:37 +0000

    Adding a missing files not committed previously.

---
+++ brightcove_cck/js/browse.js
@@ -0,0 +1,43 @@
+/**
+  * @file browse.js
+  * Creates actions - browse and remove for Video form fields.
+  */
+
+Drupal.brightcove_cck = {};
+Drupal.brightcove_cck.actions = {};
+
+Drupal.behaviors.brightcove_cck_buttons = function(context) {
+  $('.brightcove-cck-browse-button', context).click(Drupal.brightcove_cck.actions.browse);
+  $('.brightcove-cck-upload-button', context).click(Drupal.brightcove_cck.actions.upload);
+  $('.brightcove-cck-remove-button', context).click(Drupal.brightcove_cck.actions.remove);
+  // TODO: There should be a better selector for this - how do we add a class to cck element? (attributes didn't work)
+  $('.form-text.form-autocomplete').change(Drupal.brightcove_cck.actions.change);
+};
+
+Drupal.brightcove_cck.actions.change = function() {
+  $('.brightcove-cck-remove-button[rel="' + $(this).attr('id') + '"]').attr('disabled', '');
+}
+Drupal.brightcove_cck.actions.remove = function() {
+  $('#' + $(this).attr('rel')).val('');
+}
+
+Drupal.brightcove_cck.actions.browse = function() {
+  var id = $(this).attr('rel');
+  var field_name = $('#' + id).attr('rel');
+  Drupal.modalFrame.open({onSubmit: Drupal.brightcove_cck.submit(id), url: Drupal.settings.basePath + 'brightcove_cck/browse/' + Drupal.settings.brightcove_cck[field_name].node_type + '/' + Drupal.settings.brightcove_cck[field_name].field_name + '/' + Drupal.settings.brightcove_cck[field_name].nid, width: 800, height: 600, autoFit: false});
+  return false;
+}
+
+Drupal.brightcove_cck.actions.upload = function() {
+  var id = $(this).attr('rel');
+  var field_name = $('#' + id).attr('rel');
+  Drupal.modalFrame.open({onSubmit: Drupal.brightcove_cck.submit(id), url: Drupal.settings.basePath + 'brightcove_cck/upload/' + Drupal.settings.brightcove_cck[field_name].node_type + '/' + Drupal.settings.brightcove_cck[field_name].field_name + '/' + Drupal.settings.brightcove_cck[field_name].nid, width: 800, height: 600, autoFit: false});
+  return false;
+}
+
+Drupal.brightcove_cck.submit = function(settings) {
+  return function(args) {
+    $("#" + settings).val(args.selected)
+    $('.brightcove-cck-remove-button[rel="' + settings + '"]').attr('disabled', '');
+  };
+}
