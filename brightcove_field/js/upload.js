6539e18872fdc34bec7c3eec963e14257a84837d
Peter Arato <it.arato@gmail.com>
2011-06-22 11:01:56 +0200

    Port theme calls to D7. Port JS to D7.

--- brightcove_field/js/upload.js
+++ brightcove_field/js/upload.js
@@ -5,9 +5,11 @@
 
 Drupal.brightcove_field = {};
 
-Drupal.behaviors.brightcove_field_upload = function(context) {
-  $('#brightcove-field-upload-form').submit(Drupal.brightcove_field.upload);
- //  <div class="ahah-progress ahah-progress-throbber"><div class="throbber">&nbsp;</div></div>
+Drupal.behaviors.brightcove_field_upload = {
+  attach: function(context) {
+    $('#brightcove-field-upload-form').submit(Drupal.brightcove_field.upload);
+   //  <div class="ahah-progress ahah-progress-throbber"><div class="throbber">&nbsp;</div></div>
+  }
 };
 
 Drupal.brightcove_field.upload = function() {
