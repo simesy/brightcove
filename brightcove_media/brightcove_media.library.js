32dddd12825f8e839d827bdd71e6d728f7598607
Kurucz Gergely Tamás <temaruk@gmail.com>
2011-08-01 18:56:56 +0200

    Fixes some js reattach issues.

--- brightcove_media/brightcove_media.library.js
+++ brightcove_media/brightcove_media.library.js
@@ -1,6 +1,6 @@
 (function ($) {
   Drupal.brightcoveLibrary = Drupal.brightcoveLibrary || {};
-  Drupal.brightcoveLibrary.library = {};
+  Drupal.brightcoveLibrary.library = Drupal.brightcoveLibrary.library || {};
   Drupal.brightcoveLibrary.origParams = {};
   var loaded = false;
 
@@ -11,7 +11,9 @@
       var proxied = Drupal.media.browser.validateButtons;
       Drupal.media.browser.validateButtons = function() {
         if (this.id === 'media-tab-brightcove') {
-          $('<a class="button fake-ok">Submit</a>').appendTo(this).bind('click', Drupal.media.browser.submit);
+          if (!($('.fake-ok', this).length > 0)) {
+            $('<a class="button fake-ok">Submit</a>').appendTo(this).bind('click', Drupal.media.browser.submit);
+          }
           if (!($('.fake-cancel', this).length > 0)) {
             $('<a class="button fake-cancel">Cancel</a>').appendTo(this).bind('click', Drupal.media.browser.submit);
           }
@@ -20,9 +22,11 @@
         }
       };
 
-      Drupal.brightcoveLibrary.library = new Drupal.media.browser.library(Drupal.settings.media.browser.brightcove);
+      if (typeof Drupal.brightcoveLibrary.library.start != 'function') {
+        Drupal.brightcoveLibrary.library = new Drupal.media.browser.library(Drupal.settings.media.browser.brightcove);
+      }
 
-      $('#media-browser-tabset').bind('tabsselect', function (event, ui) {
+      $('#media-browser-tabset').not('.bc-processed').addClass('bc-processed').bind('tabsselect', function (event, ui) {
         if (ui.tab.hash === '#media-tab-brightcove') {
           if (!loaded) {
             var params = {};
@@ -37,7 +41,15 @@
         }
       });
 
-      $('#edit-filter').not('.processed').addClass('processed').click(function(ev) {
+/*      $('#edit-upload').not('.bc-processed').addClass('bc-processed').click(function(ev) {
+        //ev.preventDefault();
+
+        $(Drupal.brightcoveLibrary.library.renderElement).find('#media-browser-library-list li').remove();
+        Drupal.brightcoveLibrary.library.loading = true;
+        Drupal.brightcoveLibrary.library.loadMedia();
+      });*/
+
+      $('#edit-filter').not('.bc-processed').addClass('bc-processed').click(function(ev) {
         ev.preventDefault();
 
         var searchVal = $(Drupal.brightcoveLibrary.library.renderElement).find('.search-radio:checked').val();
@@ -49,11 +61,13 @@
         Drupal.brightcoveLibrary.library.loadMedia();
       });
 
-      $('#edit-reset').not('.processed').addClass('processed').click(function(ev) {
+      $('#edit-reset').not('.bc-processed').addClass('bc-processed').click(function(ev) {
         ev.preventDefault();
 
         delete Drupal.brightcoveLibrary.library.params.filter;
 
+        $(Drupal.brightcoveLibrary.library.renderElement).find('.search-radio[value=name]').attr('checked', true);
+        $(Drupal.brightcoveLibrary.library.renderElement).find('#edit-keywords').val('');
         $(Drupal.brightcoveLibrary.library.renderElement).find('#media-browser-library-list li').remove();
         Drupal.brightcoveLibrary.library.loading = true;
         Drupal.brightcoveLibrary.library.loadMedia();
