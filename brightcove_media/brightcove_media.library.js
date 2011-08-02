0884e3c1baa7e2628c7c315230c413ce1cce0907
Kurucz Gergely Tamás <temaruk@gmail.com>
2011-08-02 11:25:05 +0200

    Adds comments. Changes a global variable.

--- brightcove_media/brightcove_media.library.js
+++ brightcove_media/brightcove_media.library.js
@@ -1,13 +1,12 @@
 (function ($) {
   Drupal.brightcoveLibrary = Drupal.brightcoveLibrary || {};
   Drupal.brightcoveLibrary.library = Drupal.brightcoveLibrary.library || {};
-  Drupal.brightcoveLibrary.origParams = {};
-  var loaded = false;
+  Drupal.brightcoveLibrary.loaded = Drupal.brightcoveLibrary.loaded || false;
 
   Drupal.behaviors.brightcoveLibrary = {
     attach: function (context, settings) {
 
-      // safely override the original function
+      // Safely override the original function, using Proxy Pattern (http://docs.jquery.com/Types#Proxy_Pattern)
       var proxied = Drupal.media.browser.validateButtons;
       Drupal.media.browser.validateButtons = function() {
         if (this.id === 'media-tab-brightcove') {
@@ -22,13 +21,15 @@
         }
       };
 
+      // Check if object already exists
       if (typeof Drupal.brightcoveLibrary.library.start != 'function') {
         Drupal.brightcoveLibrary.library = new Drupal.media.browser.library(Drupal.settings.media.browser.brightcove);
       }
 
       $('#media-browser-tabset').not('.bc-processed').addClass('bc-processed').bind('tabsselect', function (event, ui) {
         if (ui.tab.hash === '#media-tab-brightcove') {
-          if (!loaded) {
+          // Prevent reloading of media list on tabselect if already loaded in media list
+          if (!Drupal.brightcoveLibrary.loaded) {
             var params = {};
             for (var p in Drupal.settings.media.browser.brightcove) {
               params[p] = Drupal.settings.media.browser.library[p];
@@ -36,7 +37,7 @@
             params.limit = 0;
 
             Drupal.brightcoveLibrary.library.start($(ui.panel), params);
-            loaded = true;
+            Drupal.brightcoveLibrary.loaded = true;
           }
         }
       });
@@ -52,24 +53,33 @@
       $('#edit-filter').not('.bc-processed').addClass('bc-processed').click(function(ev) {
         ev.preventDefault();
 
+        // Acquire filter form values
         var searchVal = $(Drupal.brightcoveLibrary.library.renderElement).find('.search-radio:checked').val();
         var keywordsVal = $(Drupal.brightcoveLibrary.library.renderElement).find('#edit-keywords').val();
+        // set library object parameters (used for ajax loading new media into list)
         Drupal.brightcoveLibrary.library.params.filter = {search: searchVal, keywords: keywordsVal};
 
+        // Remove the media list
         $(Drupal.brightcoveLibrary.library.renderElement).find('#media-browser-library-list li').remove();
+        // Set a flag so we don't make multiple concurrent AJAX calls
         Drupal.brightcoveLibrary.library.loading = true;
+        // Reload the media list
         Drupal.brightcoveLibrary.library.loadMedia();
       });
 
       $('#edit-reset').not('.bc-processed').addClass('bc-processed').click(function(ev) {
         ev.preventDefault();
 
+        // Reset filter form values
         delete Drupal.brightcoveLibrary.library.params.filter;
-
         $(Drupal.brightcoveLibrary.library.renderElement).find('.search-radio[value=name]').attr('checked', true);
         $(Drupal.brightcoveLibrary.library.renderElement).find('#edit-keywords').val('');
+
+        // Remove the media list
         $(Drupal.brightcoveLibrary.library.renderElement).find('#media-browser-library-list li').remove();
+        // Set a flag so we don't make multiple concurrent AJAX calls
         Drupal.brightcoveLibrary.library.loading = true;
+        // Reload the media list
         Drupal.brightcoveLibrary.library.loadMedia();
       });
     }
