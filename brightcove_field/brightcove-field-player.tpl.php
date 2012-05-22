b6451082abda8908de954fc27b5cc97038dada41
Attila Cs.Nagy <cs.nagy.attila@gmail.com>
2012-05-22 15:09:07 +0200

    html embed code fix

--- brightcove_field/brightcove-field-player.tpl.php
+++ brightcove_field/brightcove-field-player.tpl.php
@@ -3,23 +3,25 @@
 /**
  * @file brightcove-field-player.tpl.php
  */
+
 ?>
 
-<object id="<?php print $id;?>" width="<?php print $width;?>" class="<?php print $class;?>"
+<object id="<?php print $id;?>" width="<?php print $width;?>" class="<?php print join($classes_array, ',');?>"
      height="<?php print $height;?>" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
      codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,47,0">
-   <param name="movie" value="http://c.brightcove.com/services/viewer/federated_f9/19174954001?isVid=1" />
+   <param name="movie" value="http://c.brightcove.com/services/viewer/federated_f9?isVid=1&isUI=1" />
    <param name="bgcolor" value="<?php print $bgcolor;?>" />
    <param name="base" value="http://admin.brightcove.com" />
    <param name="seamlesstabbing" value="false" />
    <param name="allowFullScreen" value="true" />
    <param name="swLiveConnect" value="true" />
    <param name="allowScriptAccess" value="always" />
-   <param name="flashVars" value="<?php print $flashvars;?>&" />
+   <param name="flashVars" value="<?php print $flashvars;?>" />
 
    <embed src="http://c.brightcove.com/services/viewer/federated_f9/19174954001?isVid=1"
      bgcolor="<?php print $bgcolor;?>"
-     flashVars="<?php print $flashvars;?>&"
+     flashVars="<?php print $flashvars;?>"
+     name='flashObj'
      wmode="transparent"
      base="http://admin.brightcove.com"
      width="<?php print $width;?>"
