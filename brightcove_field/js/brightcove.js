f1c015c319a2b657fb9874822431b20f40c8c5a7
Tamás Demeter-Haludka <yorirou@gmail.com>
2015-05-05 10:29:51 +0200

    Fixes the Browser/Upload button issue.

--- brightcove_field/js/brightcove.js
+++ brightcove_field/js/brightcove.js
@@ -62,92 +62,91 @@
   };
 
   Drupal.ajax.prototype.commands.ui_dialog = function (ajax, response, status) {
-      var wrapper = response.selector ? $(response.selector) : $(ajax.wrapper);
-      var effect = ajax.getEffect(response);
-      var new_content;
-      var title = response.title;
-      var loading;
+    var wrapper = response.selector ? $(response.selector) : $(ajax.wrapper);
+    var effect = ajax.getEffect(response);
+    var new_content;
+    var title = response.title;
+    var loading;
 
-      var settings = response.settings || ajax.settings || Drupal.settings;
-      Drupal.brightcove_field.dialog_field_rel = response.field_rel;
-      Drupal.detachBehaviors(wrapper, settings);
+    var settings = response.settings || ajax.settings || Drupal.settings;
+    Drupal.brightcove_field.dialog_field_rel = response.field_rel;
+    Drupal.detachBehaviors(wrapper, settings);
 
-      if (Drupal.brightcove_field.dialog === null) {
-        if (response.iframe) {
-          loading = true;
-          new_content = $('<iframe id="' + response.id + '-iframe"/>').attr('src', response.data);
-          new_content.load(function() {
-            try {
-              Drupal.brightcove_field.dialog.dialog('option', 'title', title);
-            } catch(e) {}
-          });
-        }
-        else {
-          loading = false;
-          var new_content_wrapped = $('<div></div>').html(response.data);
-          new_content = new_content_wrapped.contents();
-
-          if (new_content.length !== 1 || new_content.get(0).nodeType !== 1) {
-            new_content = new_content_wrapped;
-          }
-        }
-
-        var content_height = settings.height ? settings.height : 600 - 75 ;
-        var content_width = settings.width ? settings.width + 'px' : 100 + '%';
-        var dialog_height = settings.height ? (parseInt(settings.height) + 70) : 600;
-        var dialog_width = settings.width ? (parseInt(settings.width) + 30) : 950;
-
-        // Add the new content to the page.
-        //wrapper[method](new_content);
-        Drupal.brightcove_field.dialog = wrapper.dialog({
-          autoOpen: true,
-          height: dialog_height,
-          width: dialog_width,
-          modal: true,
-          resizable: false,
-          show: 'fade',
-          hide: 'fade',
-          title: loading ? 'Loading...' : title,
-          dialogClass: response.id,
-          open: function() {
-            $(this).html(new_content);
-            $(this).attr('rel', Drupal.brightcove_field.dialog_field_rel);
-            new_content.attr('height', content_height - (settings.height ? 0 : 5) + 'px');
-            new_content.attr('width', content_width);
-          },
-          close: function() {
-            Drupal.brightcove_field.dialog_field_rel = null;
-            Drupal.brightcove_field.dialog = null;
-            $(this).remove();
-          }
+    if (Drupal.brightcove_field.dialog === null) {
+      if (response.iframe) {
+        loading = true;
+        new_content = $('<iframe id="' + response.id + '-iframe"/>').attr('src', response.data);
+        new_content.load(function() {
+          try {
+            Drupal.brightcove_field.dialog.dialog('option', 'title', title);
+          } catch(e) {}
         });
+      }
+      else {
+        loading = false;
+        var new_content_wrapped = $('<div></div>').html(response.data);
+        new_content = new_content_wrapped.contents();
 
-        // Immediately hide the new content if we're using any effects.
-        if (effect.showEffect !== 'show') {
-          new_content.hide();
-        }
-
-        // Determine which effect to use and what content will receive the
-        // effect, then show the new content.
-        if ($('.ajax-new-content', new_content).length > 0) {
-          $('.ajax-new-content', new_content).hide();
-          new_content.show();
-          $('.ajax-new-content', new_content)[effect.showEffect](effect.showSpeed);
-        }
-        else if (effect.showEffect !== 'show') {
-          new_content[effect.showEffect](effect.showSpeed);
-        }
-
-        // Attach all JavaScript behaviors to the new content, if it was successfully
-        // added to the page, this if statement allows #ajax['wrapper'] to be
-        // optional.
-        if (new_content.parents('html').length > 0) {
-          // Apply any settings from the returned JSON if available.
-          settings = response.settings || ajax.settings || Drupal.settings;
-          Drupal.attachBehaviors(new_content.contents(), settings);
+        if (new_content.length !== 1 || new_content.get(0).nodeType !== 1) {
+          new_content = new_content_wrapped;
         }
       }
 
+      var content_height = settings.height ? settings.height : 600 - 75 ;
+      var content_width = settings.width ? settings.width + 'px' : 100 + '%';
+      var dialog_height = settings.height ? (parseInt(settings.height) + 70) : 600;
+      var dialog_width = settings.width ? (parseInt(settings.width) + 30) : 950;
+
+      // Add the new content to the page.
+      //wrapper[method](new_content);
+      Drupal.brightcove_field.dialog = wrapper.dialog({
+        autoOpen: true,
+        height: dialog_height,
+        width: dialog_width,
+        modal: true,
+        resizable: false,
+        show: 'fade',
+        hide: 'fade',
+        title: loading ? 'Loading...' : title,
+        dialogClass: response.id,
+        open: function() {
+          $(this).html(new_content);
+          $(this).attr('rel', Drupal.brightcove_field.dialog_field_rel);
+          new_content.attr('height', content_height - (settings.height ? 0 : 5) + 'px');
+          new_content.attr('width', content_width);
+        },
+        close: function() {
+          Drupal.brightcove_field.dialog_field_rel = null;
+          Drupal.brightcove_field.dialog = null;
+          $(this).remove();
+        }
+      });
+
+      // Immediately hide the new content if we're using any effects.
+      if (effect.showEffect !== 'show') {
+        new_content.hide();
+      }
+
+      // Determine which effect to use and what content will receive the
+      // effect, then show the new content.
+      if ($('.ajax-new-content', new_content).length > 0) {
+        $('.ajax-new-content', new_content).hide();
+        new_content.show();
+        $('.ajax-new-content', new_content)[effect.showEffect](effect.showSpeed);
+      }
+      else if (effect.showEffect !== 'show') {
+        new_content[effect.showEffect](effect.showSpeed);
+      }
+
+      // Attach all JavaScript behaviors to the new content, if it was successfully
+      // added to the page, this if statement allows #ajax['wrapper'] to be
+      // optional.
+      if (new_content.parents('html').length > 0) {
+        // Apply any settings from the returned JSON if available.
+        settings = response.settings || ajax.settings || Drupal.settings;
+        Drupal.attachBehaviors(new_content.contents(), settings);
+      }
+    }
     };
 
     Drupal.ajax.prototype.commands.ui_dialog_close = function (ajax, response, status) {
