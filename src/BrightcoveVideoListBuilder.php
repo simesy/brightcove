a9c00e27734e751d15b25210af4ed29413cd6ccf
Tamás NAGY <ycecube@gmail.com>
2016-04-27 13:04:21 +0200

    Add more columns for the videos list R2184

--- src/BrightcoveVideoListBuilder.php
+++ src/BrightcoveVideoListBuilder.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\brightcove;
 
+use Drupal\Core\Datetime\DateFormatter;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
 use Drupal\Core\Entity\EntityStorageInterface;
@@ -14,6 +15,8 @@ use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Routing\LinkGeneratorTrait;
 use Drupal\Core\Session\AccountProxy;
 use Drupal\Core\Url;
+use Drupal\file\Entity\File;
+use Drupal\image\Entity\ImageStyle;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -32,15 +35,24 @@ class BrightcoveVideoListBuilder extends EntityListBuilder {
   protected $accountProxy;
 
   /**
+   * Date formatter.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatter
+   */
+  protected $dateFormatter;
+
+  /**
    * Constructor.
    *
    * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
    * @param \Drupal\Core\Entity\EntityStorageInterface $storage
    * @param \Drupal\Core\Session\AccountProxy $account_proxy
+   * @param \Drupal\Core\Datetime\DateFormatter $date_formatter
    */
-  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, AccountProxy $account_proxy) {
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, AccountProxy $account_proxy, DateFormatter $date_formatter) {
     parent::__construct($entity_type, $storage);
     $this->accountProxy = $account_proxy;
+    $this->dateFormatter = $date_formatter;
   }
 
   /**
@@ -50,8 +62,23 @@ class BrightcoveVideoListBuilder extends EntityListBuilder {
     return new static(
       $entity_type,
       $container->get('entity_type.manager')->getStorage($entity_type->id()),
-      $container->get('current_user')
+      $container->get('current_user'),
+      $container->get('date.formatter')
     );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEntityIds() {
+    $query = $this->getStorage()->getQuery()
+      ->sort('changed', 'DESC');
+
+    // Only add the pager if a limit is specified.
+    if ($this->limit) {
+      $query->pager($this->limit);
+    }
+    return $query->execute();
   }
 
   /**
@@ -60,8 +87,12 @@ class BrightcoveVideoListBuilder extends EntityListBuilder {
   public function buildHeader() {
     // Assemble header columns.
     $header = [
-      'id' => $this->t('Brightcove Video ID'),
+      'video' => $this->t('Video'),
       'name' => $this->t('Name'),
+      'status' => $this->t('Status'),
+      'updated' => $this->t('Updated'),
+      'reference_id' => $this->t('Reference ID'),
+      'created' => $this->t('Created'),
     ];
 
     // Add operations header column only if the user has access.
@@ -91,10 +122,32 @@ class BrightcoveVideoListBuilder extends EntityListBuilder {
       $name = $entity->label();
     }
 
+    // Get thumbnail image style and render it.
+    $thumbnail = $entity->getThumbnail();
+    $thumbnail_image = '';
+    if (!empty($thumbnail['target_id'])) {
+      /** @var \Drupal\file\Entity\File $thumbnail_file */
+      $thumbnail_file = File::load($thumbnail['target_id']);
+      /** @var \Drupal\image\Entity\ImageStyle $image_style */
+      $image_style = ImageStyle::load('brightcove_videos_list_thumbnail');
+      if (!is_null($image_style)) {
+        $image_uri = $image_style->buildUrl($thumbnail_file->getFileUri());
+        $thumbnail_image = "<img src='{$image_uri}' alt='{$entity->getName()}'>";
+      }
+    }
+
     // Assemble row.
     $row = [
-      'id' => $entity->id(),
+      'video' => [
+        'data' => [
+          '#markup' => $thumbnail_image,
+        ],
+      ],
       'name' => $name,
+      'status' => $entity->isPublished() ? $this->t('Active') : $this->t('Inactive'),
+      'updated' => $this->dateFormatter->format($entity->getChangedTime(), 'short'),
+      'reference_id' => $entity->getReferenceID(),
+      'created' => $this->dateFormatter->format($entity->getCreatedTime(), 'short'),
     ];
 
     // Add operations column only if the user has access.
