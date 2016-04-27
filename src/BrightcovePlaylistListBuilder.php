71c6e3836f98b5ae2bfd126d7e159ea15133ad5e
Tamás NAGY <ycecube@gmail.com>
2016-04-27 10:04:12 +0200

    Handle permissions on video/playlist list pages R2180

--- src/BrightcovePlaylistListBuilder.php
+++ src/BrightcovePlaylistListBuilder.php
@@ -9,8 +9,12 @@ namespace Drupal\brightcove;
 
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Routing\LinkGeneratorTrait;
+use Drupal\Core\Session\AccountProxy;
 use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines a class to build a listing of Brightcove Playlists.
@@ -19,13 +23,53 @@ use Drupal\Core\Url;
  */
 class BrightcovePlaylistListBuilder extends EntityListBuilder {
   use LinkGeneratorTrait;
+
+  /**
+   * Account proxy.
+   *
+   * @var \Drupal\Core\Session\AccountProxy
+   */
+  protected $accountProxy;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   * @param \Drupal\Core\Session\AccountProxy $account_proxy
+   */
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, AccountProxy $account_proxy) {
+    parent::__construct($entity_type, $storage);
+    $this->accountProxy = $account_proxy;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity_type.manager')->getStorage($entity_type->id()),
+      $container->get('current_user')
+    );
+  }
+
   /**
    * {@inheritdoc}
    */
   public function buildHeader() {
-    $header['id'] = $this->t('Brightcove Playlist ID');
-    $header['name'] = $this->t('Name');
-    return $header + parent::buildHeader();
+    // Assemble header columns.
+    $header = [
+      'id' => $this->t('Brightcove Playlist ID'),
+      'name' => $this->t('Name'),
+    ];
+
+    // Add operations header column only if the user has access.
+    if ($this->accountProxy->hasPermission('edit brightcove playlists') || $this->accountProxy->hasPermission('delete brightcove playlists')) {
+      $header += parent::buildHeader();
+    }
+
+    return $header;
   }
 
   /**
@@ -33,16 +77,32 @@ class BrightcovePlaylistListBuilder extends EntityListBuilder {
    */
   public function buildRow(EntityInterface $entity) {
     /* @var $entity \Drupal\brightcove\Entity\BrightcovePlaylist */
-    $row['id'] = $entity->id();
-    $row['name'] = $this->l(
-      $entity->label(),
-      new Url(
-        'entity.brightcove_playlist.canonical', array(
-          'brightcove_playlist' => $entity->id(),
+    if (($entity->isPublished() && $this->accountProxy->hasPermission('view published brightcove playlists')) || (!$entity->isPublished() && $this->accountProxy->hasPermission('view unpublished brightcove playlists'))) {
+      $name = $this->l(
+        $entity->label(),
+        new Url(
+          'entity.brightcove_playlist.canonical', array(
+            'brightcove_playlist' => $entity->id(),
+          )
         )
-      )
-    );
-    return $row + parent::buildRow($entity);
+      );
+    }
+    else {
+      $name = $entity->label();
+    }
+
+    // Assemble row.
+    $row = [
+      'id' => $entity->id(),
+      'name' => $name,
+    ];
+
+    // Add operations column only if the user has access.
+    if ($this->accountProxy->hasPermission('edit brightcove playlists') || $this->accountProxy->hasPermission('delete brightcove playlists')) {
+      $row += parent::buildRow($entity);
+    }
+
+    return $row;
   }
 
 }
--- src/BrightcoveVideoListBuilder.php
+++ src/BrightcoveVideoListBuilder.php
@@ -9,8 +9,12 @@ namespace Drupal\brightcove;
 
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Routing\LinkGeneratorTrait;
+use Drupal\Core\Session\AccountProxy;
 use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines a class to build a listing of Brightcove Videos.
@@ -19,13 +23,53 @@ use Drupal\Core\Url;
  */
 class BrightcoveVideoListBuilder extends EntityListBuilder {
   use LinkGeneratorTrait;
+
+  /**
+   * Account proxy.
+   *
+   * @var \Drupal\Core\Session\AccountProxy
+   */
+  protected $accountProxy;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   * @param \Drupal\Core\Session\AccountProxy $account_proxy
+   */
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, AccountProxy $account_proxy) {
+    parent::__construct($entity_type, $storage);
+    $this->accountProxy = $account_proxy;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity_type.manager')->getStorage($entity_type->id()),
+      $container->get('current_user')
+    );
+  }
+
   /**
    * {@inheritdoc}
    */
   public function buildHeader() {
-    $header['id'] = $this->t('Brightcove Video ID');
-    $header['name'] = $this->t('Name');
-    return $header + parent::buildHeader();
+    // Assemble header columns.
+    $header = [
+      'id' => $this->t('Brightcove Video ID'),
+      'name' => $this->t('Name'),
+    ];
+
+    // Add operations header column only if the user has access.
+    if ($this->accountProxy->hasPermission('edit brightcove videos') || $this->accountProxy->hasPermission('delete brightcove videos')) {
+      $header += parent::buildHeader();
+    }
+
+    return $header;
   }
 
   /**
@@ -33,16 +77,31 @@ class BrightcoveVideoListBuilder extends EntityListBuilder {
    */
   public function buildRow(EntityInterface $entity) {
     /* @var $entity \Drupal\brightcove\Entity\BrightcoveVideo */
-    $row['id'] = $entity->id();
-    $row['name'] = $this->l(
-      $entity->label(),
-      new Url(
-        'entity.brightcove_video.canonical', array(
-          'brightcove_video' => $entity->id(),
+    if (($entity->isPublished() && $this->accountProxy->hasPermission('view published brightcove videos')) || (!$entity->isPublished() && $this->accountProxy->hasPermission('view unpublished brightcove videos'))) {
+      $name = $this->l(
+        $entity->label(),
+        new Url(
+          'entity.brightcove_video.canonical', array(
+            'brightcove_video' => $entity->id(),
+          )
         )
-      )
-    );
-    return $row + parent::buildRow($entity);
-  }
+      );
+    }
+    else {
+      $name = $entity->label();
+    }
 
+    // Assemble row.
+    $row = [
+      'id' => $entity->id(),
+      'name' => $name,
+    ];
+
+    // Add operations column only if the user has access.
+    if ($this->accountProxy->hasPermission('edit brightcove videos') || $this->accountProxy->hasPermission('delete brightcove videos')) {
+      $row += parent::buildRow($entity);
+    }
+
+    return $row;
+  }
 }
